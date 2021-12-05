01d2aaeaf700afd49d74c5ddd07796d32494d18b
Alberto Williams <68757297+alberto1996-23@users.noreply.github.com>
2021-12-05 17:55:44 -0600

    updates on contact page and CSS

---
+++ beto.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Global variables
+var importantStuff = {
+
+};
+
+// Functions, Classes
+var someFunction = function() {
+    //blee-blah
+};
+
+//Down to business
+someFunction(importantStuff);
+needyStuff();
+// Function expressions
+function needyStuff(){
+    //blee-blah
+};
